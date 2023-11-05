c31b3b04fe62986804115722f25b115b3b010524
Gabrielap000 <mg.paez@uniandes.edu.co>
2023-11-05 16:03:07 -0500

    ProfileView

---
+++ SafeGo/SafeGo/View/ChangeAddressesView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeAddressesView.swift
+//  SafeGo
+//
+//  Created by Gabriela on 11/5/23.
+//
+
+import SwiftUI
+
+struct ChangeAddressesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeAddressesView()
+}
--- SafeGo/SafeGo/View/ProfileView.swift
+++ SafeGo/SafeGo/View/ProfileView.swift
@@ -8,8 +8,73 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var profileImage: Image = Image(systemName: "person.circle")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color(hex: 0x96CEB4)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    
+                    Button(action: {
+                        // Allow the user to change the profile picture here.
+                        // You can implement this using an ImagePicker or your preferred method.
+                    }) {
+                        profileImage
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    
+                    NavigationLink(destination: TravelsView()) {
+                                            RoundedRectangle(cornerSize: CGSize(width: 25, height: 25))
+                                                .foregroundColor(Color(hex: 0xCFF2E5))
+                                                .frame(width: UIScreen.main.bounds.width / 1.1, height: UIScreen.main.bounds.height / 12)
+                                                .overlay(
+                                                    HStack {
+                                                        Image(systemName: "calendar")
+                                                            .font(.system(size: 60))
+                                                            .frame(width: UIScreen.main.bounds.width / 10, height: UIScreen.main.bounds.height / 10)
+                                                            .padding(.horizontal, UIScreen.main.bounds.width / 15)
+                                                            .foregroundColor(.black)
+                                                        Text("Travel History")
+                                                            .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 35))
+                                                            .foregroundColor(.gray)
+                                                    }
+                                                    .frame(width: UIScreen.main.bounds.width / 1.2, alignment: .leading)
+                                                )
+                                                .shadow(radius: 10)
+                                        }
+                    
+                    // Change Address Button
+                    NavigationLink(destination: ChangeAddressesView()) {
+                                           RoundedRectangle(cornerSize: CGSize(width: 25, height: 25))
+                                               .foregroundColor(Color(hex: 0xCFF2E5))
+                                               .frame(width: UIScreen.main.bounds.width / 1.1, height: UIScreen.main.bounds.height / 12)
+                                               .overlay(
+                                                   HStack {
+                                                       Image(systemName: "location")
+                                                           .font(.system(size: 60))
+                                                           .frame(width: UIScreen.main.bounds.width / 10, height: UIScreen.main.bounds.height / 10)
+                                                           .padding(.horizontal, UIScreen.main.bounds.width / 15)
+                                                           .foregroundColor(.black)
+                                                       Text("Change Address")
+                                                           .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 35))
+                                                           .foregroundColor(.gray)
+                                                   }
+                                                   .frame(width: UIScreen.main.bounds.width / 1.2, alignment: .leading)
+                                               )
+                                               .shadow(radius: 10)
+                                       }
+                    
+                    Spacer()
+                }
+            }
+            .navigationBarTitle("Profile")
+        }
     }
 }
 
