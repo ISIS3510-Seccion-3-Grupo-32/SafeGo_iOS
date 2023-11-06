9a13bb4fad78fb9e27aa104a22eb3fc6d9bc32f2
jfpatino01 <64269726+jfpatino01@users.noreply.github.com>
2023-11-05 21:32:18 -0500

    upgrade looks and coloring

--- SafeGo/SafeGo/View/SettingsView.swift
+++ SafeGo/SafeGo/View/SettingsView.swift
@@ -15,62 +15,132 @@ struct SettingsView: View {
 
     var body: some View {
         NavigationView {
-            ZStack {
-                Color(hex: 0x96CEB4)
-                    .ignoresSafeArea()
-
                 VStack {
-                    Spacer()
-                    Section(header: Text("General Settings")) {
-                        Toggle("Dark Mode", isOn: $isDarkMode)
-                        Toggle("Notifications", isOn: $areNotificationsEnabled)
+                    HStack()
+                    {
+                        NavigationLink(destination: HomeView()
+                            .navigationBarBackButtonHidden(true))
+                        {
+                            Image(systemName: "house.fill")
+                                .foregroundColor(.black)
+                                .font(.system(size: 40))
+                        }
+                        .navigationBarBackButtonHidden(true)
+                        
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        NavigationLink(destination: UserComplaintsView()
+                            .navigationBarBackButtonHidden(true))
+                        {
+                            Image(systemName: "line.horizontal.3")
+                                .foregroundColor(.black)
+                                .font(.system(size: 40))
+                        }
                     }
+                    .frame(width: UIScreen.main.bounds.width / 1.2)
+                    
+                    
+                    Spacer()
+                    Section(header: Text("General Settings")
+                        .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 25))
+                        .foregroundColor(.white)
+                            
+                    ) {
+                        
+                        Toggle("Dark Mode", isOn: $isDarkMode)
+                            .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 35))
+                            .foregroundColor(.white)
+                            .tint(.blue)
+
+                        Toggle("Notifications", isOn: $areNotificationsEnabled)
+                            .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 35))
+                            .foregroundColor(.white)
+                            .tint(.blue)
+
+                    }
+                    .frame(width: UIScreen.main.bounds.width / 1.2)
                     Spacer()
                     Spacer()
 
-                    Section(header: Text("Sound")) {
+                    Section(header: Text("Sound")
+                        .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 35))
+                        .foregroundColor(.white)
+
+                    ) {
                         Slider(value: $soundLevel, in: 0...1, step: 0.01)
+                            .tint(.blue)
                     }
+                    .frame(width: UIScreen.main.bounds.width / 1.2)
                     
                     Spacer()
                     Spacer()
 
-                    Section(header: Text("Language")) {
+                    Section(header: Text("Language")
+                        .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 35))
+                        .foregroundColor(.white)
+                        ) {
                         Picker("Language", selection: $selectedLanguage) {
                             Text("English").tag("English")
+                                .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 40))
+                                .foregroundColor(.white)
+
                             Text("Español").tag("Español")
+                                .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 40))
+                                .foregroundColor(.white)
+
+
                             Text("French").tag("French")
+                                .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 40))
+                                .foregroundColor(.white)
+
                         }
                         .pickerStyle(SegmentedPickerStyle())
+                        .frame(width: UIScreen.main.bounds.width / 1.2)
+
                     }
                     
                     Spacer()
                     Spacer()
+                    Spacer()
+                    Spacer()
+
 
                     Button("Save") {
-
                         UserDefaults.standard.set(isDarkMode, forKey: "isDarkMode")
                         UserDefaults.standard.set(areNotificationsEnabled, forKey: "areNotificationsEnabled")
                         UserDefaults.standard.set(soundLevel, forKey: "soundLevel")
                         UserDefaults.standard.set(selectedLanguage, forKey: "selectedLanguage")
                     }
-                    .frame(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.height / 15)
+                    
+                    
+                    .frame(width: UIScreen.main.bounds.width / 2.5, height: UIScreen.main.bounds.height / 20)
                     .background(Color(hex: 0xCFF2E5))
                     .cornerRadius(10)
                     .foregroundColor(.black)
-                    .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 35)) // Missing closing parenthesis here
+                    .font(.custom("DM Sans", size: UIScreen.main.bounds.height / 35))
                     Spacer()
                     Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+
                 }
-                .navigationBarTitle("Settings")
-            }
-        }
-        .onAppear {
-            
-            isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
-            areNotificationsEnabled = UserDefaults.standard.bool(forKey: "areNotificationsEnabled")
-            soundLevel = UserDefaults.standard.double(forKey: "soundLevel")
-            selectedLanguage = UserDefaults.standard.string(forKey: "selectedLanguage") ?? "English"
+                .onAppear {
+                    
+                    isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
+                    areNotificationsEnabled = UserDefaults.standard.bool(forKey: "areNotificationsEnabled")
+                    soundLevel = UserDefaults.standard.double(forKey: "soundLevel")
+                    selectedLanguage = UserDefaults.standard.string(forKey: "selectedLanguage") ?? "English"
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(hex: 0x96CEB4))
+
         }
     }
 }
