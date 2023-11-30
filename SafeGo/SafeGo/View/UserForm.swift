ab821bb814345d4d54c7d35a1b42c50d40de5162
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2023-11-30 16:21:49 -0500

    Form created

--- SafeGo/SafeGo/View/UserForm.swift
+++ SafeGo/SafeGo/View/UserForm.swift
@@ -11,49 +11,59 @@ struct UserForm: View {
     @ObservedObject var controller = UserFormController()
 
     var body: some View {
-        Form {
-            Section(header: Text("Safe Navigation")) {
-                Picker("Most Used Device", selection: $controller.preferences.deviceForSafeNavigation) {
-                    ForEach(controller.devices, id: \.self) { device in
-                        Text(device).tag(device)
+        VStack{
+            
+            Text("USER'S FORM")
+                .font(.title) // You can adjust the size based on your preference
+                .bold()
+                .padding()
+
+            Text("Which are the devices that you use the most for the followig funcitnalities?").padding()
+            Form {
+                Section(header: Text("Safe Navigation").bold()) {
+                    Picker("Most Used Device", selection: $controller.preferences.deviceForSafeNavigation) {
+                        ForEach(controller.devices, id: \.self) { device in
+                            Text(device).tag(device)
+                        }
+                    }
+                }
+
+                Section(header: Text("Report a Crime").bold()) {
+                    Picker("Most Used Device", selection: $controller.preferences.deviceForReportCrime) {
+                        ForEach(controller.devices, id: \.self) { device in
+                            Text(device).tag(device)
+                        }
+                    }
+                }
+
+                Section(header: Text("Login").bold()) {
+                    Picker("Most Used Device", selection: $controller.preferences.deviceForLogin) {
+                        ForEach(controller.devices, id: \.self) { device in
+                            Text(device).tag(device)
+                        }
+                    }
+                }
+
+                Section(header: Text("Send a Suggestion").bold()) {
+                    Picker("Most Used Device", selection: $controller.preferences.deviceForSuggestion) {
+                        ForEach(controller.devices, id: \.self) { device in
+                            Text(device).tag(device)
+                        }
+                    }
+                }
+
+                Section {
+                    Button(action: {
+                        self.controller.submitForm()
+                    }) {
+                        Text("Submit")
                     }
                 }
             }
-
-            Section(header: Text("Report a Crime")) {
-                Picker("Most Used Device", selection: $controller.preferences.deviceForReportCrime) {
-                    ForEach(controller.devices, id: \.self) { device in
-                        Text(device).tag(device)
-                    }
-                }
-            }
-
-            Section(header: Text("Login")) {
-                Picker("Most Used Device", selection: $controller.preferences.deviceForLogin) {
-                    ForEach(controller.devices, id: \.self) { device in
-                        Text(device).tag(device)
-                    }
-                }
-            }
-
-            Section(header: Text("Send a Suggestion")) {
-                Picker("Most Used Device", selection: $controller.preferences.deviceForSuggestion) {
-                    ForEach(controller.devices, id: \.self) { device in
-                        Text(device).tag(device)
-                    }
-                }
-            }
-
-            Section {
-                Button(action: {
-                    self.controller.submitForm()
-                }) {
-                    Text("Submit")
-                }
-            }
+            .scrollContentBackground(.hidden)
+            .navigationTitle("User Preferences")
         }
-        .padding()
-        .navigationTitle("User Preferences")
+        .background(Color(hex: 0x96CEB4))
     }
 }
 
