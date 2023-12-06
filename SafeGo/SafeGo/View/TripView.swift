bf4a2524d96c4697e214adeb5eea5b573743f6f8
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2023-12-06 15:16:35 -0500

    Almost done :D

--- SafeGo/SafeGo/View/TripView.swift
+++ SafeGo/SafeGo/View/TripView.swift
@@ -15,65 +15,101 @@ struct TripView: View
     let icon: String
 
     var body: some View {
-        NavigationView {
-            VStack() {
-                HStack() {
-                    NavigationLink(destination: HomeView()
-                        .navigationBarBackButtonHidden(true)) {
-                        Image(systemName: "house.fill")
-                            .foregroundColor(.black)
-                            .font(.system(size: 40))
-                    }
+        Group {
+            if connectivityViewModel.isConnected {
+                NavigationView {
+                    VStack() {
+                        HStack() {
+                            NavigationLink(destination: HomeView()
+                                .navigationBarBackButtonHidden(true)) {
+                                Image(systemName: "house.fill")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 40))
+                            }
 
-                    Spacer()
-                    NavigationLink(destination: UserComplaintsView()
-                        .navigationBarBackButtonHidden(true)) {
-                        Image(systemName: "line.horizontal.3")
-                            .foregroundColor(.black)
-                            .font(.system(size: 40))
+                            Spacer()
+                            NavigationLink(destination: UserComplaintsView()
+                                .navigationBarBackButtonHidden(true)) {
+                                Image(systemName: "line.horizontal.3")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 40))
+                            }
+                        }
+                        .frame(width: UIScreen.main.bounds.width / 1.2)
+                        Spacer()
+                        VStack {
+                            Spacer()
+                            Spacer()
+                            HStack {
+                                Image(systemName: "clock")
+                                    .font(.system(size: 24))
+                                Text("You will get there at")
+                                    .font(.system(size: 20))
+                                    .foregroundColor(.black)
+                            }
+                            Spacer()
+
+                            if let address = address {
+                                Text("Destination")
+                                    .font(.system(size: 20))
+                                    .bold()
+
+                                Text(address) // Display the saved address
+                                    .font(.system(size: 20))
+                                    .bold()
+                                
+                            } else {
+                                Text("No address saved") // Display a message if no address is available
+                                    .font(.system(size: 20))
+                                    .bold()
+                            }
+                        }
+                        Spacer()
+                        Spacer()
+
+                        HStack {
+                            ButtonFactory.createButton(title: "Go") {
+                            }
+                        }
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color(hex: 0x96CEB4))
                 }
-                .frame(width: UIScreen.main.bounds.width / 1.2)
-                Spacer()
-                VStack {
-                    Spacer()
-                    Spacer()
-                    HStack {
-                        Image(systemName: "clock")
-                            .font(.system(size: 24))
-                        Text("You will get there at")
-                            .font(.system(size: 20))
-                            .foregroundColor(.black)
-                    }
-                    Spacer()
 
-                    if let address = address {
-                        Text("Destination")
-                            .font(.system(size: 20))
-                            .bold()
-
-                        Text(address) // Display the saved address
-                            .font(.system(size: 20))
-                            .bold()
-                        
-                    } else {
-                        Text("No address saved") // Display a message if no address is available
-                            .font(.system(size: 20))
-                            .bold()
-                    }
-                }
-                Spacer()
-                Spacer()
-
-                HStack {
-                    ButtonFactory.createButton(title: "Go") {
-                    }
-                }
+            } else {
+                NoConnectionView()
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color(hex: 0x96CEB4))
         }
-        
+        .onAppear {
+            connectivityViewModel.isConnected = connectivityViewController.isConnectedToInternet()
+        }
     }
 }
 
